57572dec9e90fb3fdb6a4b8dc9b599d84908aa4d
unexcellent <>
2024-09-23 20:40:27 +0200

    chore: set up CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+mod templates;
+
+/// Program for generating a resume from JSONResume data.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the data describing your resume. It needs to comply with theJSONResume schema (see https://jsonresume.org/).
+    resume_data_path: PathBuf,
+
+    /// Location where the generated PDF should be stored.
+    target_path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Args::parse();
 }
