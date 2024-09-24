9e82e6ac4912975024676ef4bb39fb887ee727ad
unexcellent <>
2024-09-24 12:30:47 +0200

    refactor: extract functionality into separate function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod generate_pdf;
 mod load_json_resume;
 mod resolve_image_path;
 mod save_to_pdf;
@@ -6,15 +7,12 @@ mod templates;
 use std::path::PathBuf;
 
 use clap::Parser;
-use load_json_resume::load_json_resume;
-use resolve_image_path::resolve_image_path;
-use save_to_pdf::save_to_pdf;
-use templates::Coruscant;
+use generate_pdf::generate_pdf;
 
 /// Program for generating a resume from JSONResume data.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
+pub struct Args {
     /// Path to the data describing your resume. It needs to comply with theJSONResume schema (see https://jsonresume.org/).
     resume_data_path: PathBuf,
 
@@ -24,15 +22,5 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-
-    let mut resume_data = load_json_resume(&args.resume_data_path).unwrap();
-
-    if let Some(ref mut basics) = resume_data.basics {
-        basics.image = resolve_image_path(&args.resume_data_path, &basics.image);
-    }
-
-    let template = Coruscant::new(resume_data);
-
-    let html_resume = template.build();
-    save_to_pdf(html_resume, &args.target_path).expect("Failed to write to PDF.");
+    generate_pdf(args.resume_data_path, args.target_path)
 }
