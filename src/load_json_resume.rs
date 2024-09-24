28ca3c3b671cc3880d38961cc361dc151dc0435f
unexcellent <>
2024-09-24 09:37:57 +0200

    lint: use formatter

--- src/load_json_resume.rs
+++ src/load_json_resume.rs
@@ -1,7 +1,7 @@
 use std::{error::Error, fs, path::PathBuf};
 
 pub fn load_json_resume(path: &PathBuf) -> Result<json_resume::Resume, Box<dyn Error>> {
-    let contents = fs::read_to_string(&path)?;
+    let contents = fs::read_to_string(path)?;
     let resume_data = match path.extension().unwrap().to_str() {
         Some("json") => serde_json::from_str(&contents)?,
         Some("yaml") | Some("yml") => serde_yaml::from_str(&contents)?,
