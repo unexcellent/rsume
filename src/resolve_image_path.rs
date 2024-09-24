65dfe8fd997f29de641aaf27c1df26708b43c34a
unexcellent <>
2024-09-24 14:10:01 +0200

    feat: canonicalize path

--- src/resolve_image_path.rs
+++ src/resolve_image_path.rs
@@ -1,12 +1,10 @@
-use std::path::Path;
+use std::{fs, path::Path};
 
 /// Image paths can either be given as absolute or relative paths from the JSONResume file. This function resolves relative paths.
 pub fn resolve_image_path(resume_data_path: &Path, image_path: &Option<String>) -> Option<String> {
-    image_path.as_ref().map(|path| {
-        resume_data_path
-            .parent()
+    image_path.as_ref().map(|img_path| {
+        fs::canonicalize(resume_data_path.parent().unwrap().join(img_path))
             .unwrap()
-            .join(path)
             .to_str()
             .unwrap()
             .to_string()
@@ -15,6 +13,8 @@ pub fn resolve_image_path(resume_data_path: &Path, image_path: &Option<String>) 
 
 #[cfg(test)]
 pub mod tests {
+    use tempfile::TempDir;
+
     use super::*;
     use std::path::PathBuf;
 
@@ -29,22 +29,14 @@ pub mod tests {
 
     #[test]
     fn same_folder() {
-        let resume_data_path = PathBuf::from("/path/to/some/resume.json");
+        let tmp_dir = TempDir::new().unwrap();
+        let resume_data_path = tmp_dir.path().join("resume.json");
         let image_path = Some("profile.png".to_string());
 
-        let actual = resolve_image_path(&resume_data_path, &image_path);
-        assert_eq!(actual, Some("/path/to/some/profile.png".to_string()))
-    }
-
-    #[test]
-    fn neighboring_folder() {
-        let resume_data_path = PathBuf::from("/path/to/some/resume.json");
-        let image_path = Some("../images/profile.png".to_string());
+        fs::write(&resume_data_path, "").unwrap();
+        fs::write(tmp_dir.path().join("profile.png"), "").unwrap();
 
         let actual = resolve_image_path(&resume_data_path, &image_path);
-        assert_eq!(
-            actual,
-            Some("/path/to/some/../images/profile.png".to_string())
-        )
+        assert!(PathBuf::from(actual.unwrap()).is_file())
     }
 }
