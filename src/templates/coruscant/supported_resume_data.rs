1fa3058e5747f7f1fde34151957c3be1cbafc34b
unexcellent <>
2024-09-25 15:44:40 +0200

    feat: add work highlights

--- src/templates/coruscant/supported_resume_data.rs
+++ src/templates/coruscant/supported_resume_data.rs
@@ -94,6 +94,7 @@ pub struct Work {
     pub description: String,
     pub start_date: String,
     pub end_date: String,
+    pub highlights: Vec<String>,
 }
 impl Work {
     pub fn try_from(work: json_resume::Work) -> Result<Self, String> {
@@ -102,6 +103,7 @@ impl Work {
             description: get_mandatory_field(work.description, "education.institution")?,
             start_date: get_mandatory_field(work.start_date, "education.start_date")?,
             end_date: get_mandatory_field(work.end_date, "education.end_date")?,
+            highlights: work.highlights.iter().map(|h| h.to_string()).collect(),
         })
     }
 }
--- src/templates/coruscant/work/work_wrapper.rs
+++ src/templates/coruscant/work/work_wrapper.rs
@@ -39,10 +39,28 @@ fn build_entries(work: &Vec<Work>) -> String {
             start_date: work_entry.start_date.clone(),
             end_date: work_entry.end_date.clone(),
             title: work_entry.name.clone(),
-            body: work_entry.description.clone(),
+            body: build_entry_body(work_entry),
         };
         entries_html.push_str(&entry.build());
     }
 
     entries_html
+}
+
+fn build_entry_body(work_entry: &Work) -> String {
+    let mut entry_body = String::new();
+    entry_body.push_str(&work_entry.description);
+    entry_body.push_str("\n<ul>");
+
+    for highlight in &work_entry.highlights {
+        entry_body.push_str(&format!(
+            "
+            \n<li>{highlight}</li>
+            "
+        ));
+    }
+
+    entry_body.push_str("\n</ul>");
+
+    entry_body
 }
