bd64a516b7fd5b7b25047f0740f01e2ca81fb35b
unexcellent <>
2024-09-27 20:17:56 +0200

    refactor: transfer generate_pdf() into main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-mod generate_pdf;
 mod io;
 mod templates;
 
 use std::path::PathBuf;
 
+use crate::io::load_json_resume::load_json_resume;
+use crate::io::resolve_image_path::resolve_image_path;
+use crate::io::save_to_pdf::save_to_pdf;
+use crate::templates::Coruscant;
 use clap::Parser;
-use generate_pdf::generate_pdf;
 
 /// Program for generating a resume from JSONResume data.
 #[derive(Parser, Debug)]
@@ -21,4 +23,17 @@ pub struct Args {
 fn main() {
     let args = Args::parse();
     generate_pdf(args.resume_data_path, args.target_path)
+}
+
+pub fn generate_pdf(resume_data_path: PathBuf, target_path: PathBuf) {
+    let mut resume_data = load_json_resume(&resume_data_path).unwrap();
+
+    if let Some(ref mut basics) = resume_data.basics {
+        basics.image = resolve_image_path(&resume_data_path, &basics.image);
+    }
+
+    let template = Coruscant::try_from(resume_data).unwrap();
+
+    let html_resume = template.build();
+    save_to_pdf(html_resume, &target_path).expect("Failed to write to PDF.");
 }
